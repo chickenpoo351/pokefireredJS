0842cb6568506a99950dcc821b7be091a42beb38
chickenPoo <ivanemerald1209@gmail.com>
2026-03-25 14:15:39 -0400

    First attempt at callback scaffolding they are kinda weird though... cb2 seems to be nested inside cb1...

--- src/main.js
+++ src/main.js
@@ -12,8 +12,18 @@ function checkKeys() {
     gMain.heldKeys = keys;
 }
 
+function callCallbacks() { // so currently this does nothing but I think in theory for other things which later use the callbacks this should run correctly
+    if (gMain.callback1)
+        gMain.callback1();
+}
+
+export function setMainCallback2(cb) {
+    gMain.callback2 = cb;
+} // so this will be used by many other files to you know change the callback2 into whatever is needed
+
 function gameLoop() {
     checkKeys(); // boom first part of the engine completed or well kinda...
+    callCallbacks(); // in theory should make all callbacks run I think...
     if (gMain.newKeys & Buttons.A_BUTTON)
         console.log("J (aka A button) was pressed");
     requestAnimationFrame(gameLoop); 
